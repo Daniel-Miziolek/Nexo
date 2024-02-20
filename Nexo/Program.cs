32095fd49e0f771da29427494781584b14b34149
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-02-20 18:19:53 +0100

    Update Program.cs

--- Nexo/Program.cs
+++ Nexo/Program.cs
@@ -7,21 +7,33 @@ namespace Nexo
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-
-            Lexer lexer = new Lexer(input);
-            List<Token> tokens = new List<Token>();
-
-            Token token;
-            while ((token = lexer.GetNextToken()) != null)
+            while (true)
             {
-                tokens.Add(token);
+                Console.Write(">");
+
+                string input = Console.ReadLine();
+
+                Lexer lexer = new Lexer(input);
+                List<Token> tokens = new List<Token>();
+
+                Token token;
+                while ((token = lexer.GetNextToken()) != null)
+                {
+                    tokens.Add(token);
+                }
+
+
+
+                Parser parser = new Parser(tokens);
+                parser.Parse();
+
+                if (input == "break")
+                {
+                    Console.WriteLine("The program has ended");
+                    break;
+                }
             }
-
-
-
-            Parser parser = new Parser(tokens);
-            parser.Parse();
+            
         }
     }
 
