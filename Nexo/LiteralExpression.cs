8b858640131a3dd2605942131e77cf3f400c3cba
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-03-30 11:17:41 +0100

    Add files via upload

---
+++ Nexo/LiteralExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexo2
+{
+    public class LiteralExpression : IExpression
+    {
+        public object Value { get; }
+
+        public LiteralExpression(object value)
+        {
+            Value = value;
+        }
+
+        public T Accept<T>(IExpressionVisitor<T> visitor)
+        {
+            return visitor.VisitLiteralExpression(this);
+        }
+    }
+}
