6c8d584544b62e1b94b0b3d6cb7682fa23858111
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-03-30 11:16:47 +0100

    Update Lexer.cs

--- Nexo/Lexer.cs
+++ Nexo/Lexer.cs
@@ -4,89 +4,148 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nexo
+namespace Nexo2
 {
     public class Lexer
     {
-        private readonly string _input;
-        private int _position;
+        private readonly string _source;
+        private readonly List<Token> _tokens = new List<Token>();
 
-        public Lexer(string input)
+        private int _start = 0;
+        private int _current = 0;
+        private int _line = 1;
+
+        public Lexer(string source)
         {
-            _input = input;
-            _position = 0;
+            _source = source;
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                _start = _current;
+                ScanToken();
+            }
+
+            _tokens.Add(new Token(TokenType.SemiColon, "", null));
+            return _tokens;
+        }
+
+        private void ScanToken()
+        {
+            char c = Advance();
+            switch (c)
+            {
+                case '(':
+                    AddToken(TokenType.LeftParen);
+                    break;
+                case ')':
+                    AddToken(TokenType.RightParen);
+                    break;
+                case '{':
+                    AddToken(TokenType.LeftBrace);
+                    break;
+                case '}':
+                    AddToken(TokenType.RightBrace);
+                    break;
+                case '+':
+                    AddToken(TokenType.Plus);
+                    break;
+                case '-':
+                    AddToken(TokenType.Minus);
+                    break;
+                case '*':
+                    AddToken(TokenType.Multiply);
+                    break;
+                case '/':
+                    AddToken(TokenType.Divide);
+                    break;
+                case '=':
+                    AddToken(TokenType.Equal);
+                    break;
+                case '<':
+                    AddToken(TokenType.LessThan);
+                    break;
+                case '>':
+                    AddToken(TokenType.GreaterThan);
+                    break;
+                case ';':
+                    AddToken(TokenType.SemiColon);
+                    break;
+                case ' ':
+                case '\r':
+                case '\t':
+                    // Ignore whitespace
+                    break;
+                case '\n':
+                    _line++;
+                    break;
+                default:
+                    if (char.IsDigit(c))
+                    {
+                        ScanNumber();
+                    }
+                    else if (char.IsLetter(c))
+                    {
+                        ScanIdentifier();
+                    }
+                    else
+                    {
+                        throw new Exception($"Unexpected character '{c}' at line {_line}.");
+                    }
+                    break;
+            }
+        }
+
+        private void ScanNumber()
+        {
+            while (char.IsDigit(Peek()))
+            {
+                Advance();
+            }
+
+            string number = _source.Substring(_start, _current - _start);
+            _tokens.Add(new Token(TokenType.Number, number, int.Parse(number)));
+        }
+
+        private void ScanIdentifier()
+        {
+            while (char.IsLetterOrDigit(Peek()))
+            {
+                Advance();
+            }
+
+            string text = _source.Substring(_start, _current - _start);
+            TokenType type = TokenType.Identifier;
+            if (text == "print") type = TokenType.Print;
+            else if (text == "if") type = TokenType.If;
+            else if (text == "else") type = TokenType.Else;
+
+            _tokens.Add(new Token(type, text, null));
+        }
+
+        private char Advance()
+        {
+            _current++;
+            return _source[_current - 1];
+        }
+
+        private bool IsAtEnd()
+        {
+            return _current >= _source.Length;
         }
 
         private char Peek()
         {
-            if (_position < _input.Length)
-                return _input[_position];
-            else
-                return '\0';
+            if (IsAtEnd()) return '\0';
+            return _source[_current];
         }
 
-        private char Next()
+        private void AddToken(TokenType type)
         {
-            if (_position < _input.Length)
-                return _input[_position++];
-            else
-                return '\0';
-        }
-
-        private void SkipWhitespace()
-        {
-            while (char.IsWhiteSpace(Peek()))
-            {
-                Next();
-            }
-        }
-
-        public Token GetNextToken()
-        {
-            SkipWhitespace();
-
-            if (_position >= _input.Length)
-                return null;
-
-            char currentChar = Peek();
-
-            if (char.IsLetter(currentChar))
-            {
-                return new Token(Token.Type.Word, ReadWord());
-            }
-            else if (char.IsDigit(currentChar))
-            {
-                return new Token(Token.Type.Number, ReadNumber());
-            }
-            else
-            {
-                Next();
-                return new Token(Token.Type.Symbol, currentChar.ToString());
-            }
-        }
-
-        private string ReadWord()
-        {
-            string word = "";
-
-            while (char.IsLetterOrDigit(Peek()))
-            {
-                word += Next();
-            }
-
-            return word;
-        }
-
-        private string ReadNumber()
-        {
-            string number = "";
-
-            while (char.IsDigit(Peek()))
-            {
-                number += Next();
-            }
-
-            return number;
+            string text = _source.Substring(_start, _current - _start);
+            _tokens.Add(new Token(type, text, null));
         }
     }
 }
