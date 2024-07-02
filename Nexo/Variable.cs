24429820168c88d11ca1994143f3cfc5d4a81a26
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-07-02 02:15:00 +0200

    Add files via upload

---
+++ Nexo/Variable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexo2
+{
+    public class Variable
+    {
+        public enum VarType
+        {
+            Int,
+            String
+        }
+
+        public VarType Type { get; }
+        public object Value { get; }
+
+        public Variable(int value)
+        {
+            Type = VarType.Int;
+            Value = value;
+        }
+
+        public Variable(string value)
+        {
+            Type = VarType.String;
+            Value = value;
+        }
+
+        public int AsInt()
+        {
+            if (Type != VarType.Int)
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ResetColor();
+            return (int)Value;
+        }
+
+        public string AsString()
+        {
+            if (Type != VarType.String)
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ResetColor();
+            return (string)Value;
+        }
+    }
+}
